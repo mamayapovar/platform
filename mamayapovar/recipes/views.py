25106ea0d2f39a4651d59fb38610db3a2d6b339b
syab2 <kolakrylov6@gmail.com>
2023-02-01 14:07:47 +0300

    smth

--- mamayapovar/recipes/views.py
+++ mamayapovar/recipes/views.py
@@ -1155,9 +1155,11 @@ def edit_recipe(request, id):
 def momental_search(request):
     data = {}
     query = request.POST.get('query')
-    data['categories'] = list(Category.objects.filter(name__iregex=query).values())
-    data['users'] = list(User.objects.filter(username__iregex=query).values())
-    data['recipes'] = list(Recipe.objects.filter(title__iregex=query).values())
+    if query:
+        data['categories'] = list(Category.objects.filter(name__iregex=query).values())
+        data['users'] = list(User.objects.filter(username__iregex=query).values())
+        data['recipes'] = list(Recipe.objects.filter(title__iregex=query).values())
+        return JsonResponse(data=data, status=200)
     return JsonResponse(data=data, status=200)
 
 
