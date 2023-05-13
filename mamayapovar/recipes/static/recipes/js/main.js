f119d224fcc53d60c2f5ebdeaa92b75574e70a6a
Alexey Kalinin <72299738+sonniydsgn@users.noreply.github.com>
2023-05-14 02:16:57 +0500

    добавил заглушки и фильтры, стандарт отступов

--- mamayapovar/recipes/static/recipes/js/main.js
+++ mamayapovar/recipes/static/recipes/js/main.js
@@ -32,12 +32,13 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _components_info_avatar_js__WEBPACK_IMPORTED_MODULE_11___default = /*#__PURE__*/__webpack_require__.n(_components_info_avatar_js__WEBPACK_IMPORTED_MODULE_11__);
 /* harmony import */ var _components_search_query_server_js__WEBPACK_IMPORTED_MODULE_12__ = __webpack_require__(/*! ./components/_search-query-server.js */ "./src/js/components/_search-query-server.js");
 /* harmony import */ var _components_search_query_server_js__WEBPACK_IMPORTED_MODULE_12___default = /*#__PURE__*/__webpack_require__.n(_components_search_query_server_js__WEBPACK_IMPORTED_MODULE_12__);
-/* harmony import */ var _components_edit_js__WEBPACK_IMPORTED_MODULE_13__ = __webpack_require__(/*! ./components/_edit.js */ "./src/js/components/_edit.js");
-/* harmony import */ var _components_edit_js__WEBPACK_IMPORTED_MODULE_13___default = /*#__PURE__*/__webpack_require__.n(_components_edit_js__WEBPACK_IMPORTED_MODULE_13__);
+/* harmony import */ var _components_filter_js__WEBPACK_IMPORTED_MODULE_13__ = __webpack_require__(/*! ./components/_filter.js */ "./src/js/components/_filter.js");
+/* harmony import */ var _components_filter_js__WEBPACK_IMPORTED_MODULE_13___default = /*#__PURE__*/__webpack_require__.n(_components_filter_js__WEBPACK_IMPORTED_MODULE_13__);
+/* harmony import */ var _components_edit_js__WEBPACK_IMPORTED_MODULE_14__ = __webpack_require__(/*! ./components/_edit.js */ "./src/js/components/_edit.js");
+/* harmony import */ var _components_edit_js__WEBPACK_IMPORTED_MODULE_14___default = /*#__PURE__*/__webpack_require__.n(_components_edit_js__WEBPACK_IMPORTED_MODULE_14__);
 // import './components/_bookmark.js';
 // import './components/_like.js';
 // import './components/_subscribe.js';
-// import './components/_filter.js';
 
 
 
@@ -51,6 +52,7 @@ __webpack_require__.r(__webpack_exports__);
 
 
 // import './components/_search-query.js';
+
 
 
 
@@ -104,50 +106,11 @@ __webpack_require__.r(__webpack_exports__);
 // Получение высоты шапки сайта (не забудьте вызвать функцию)
 // import { getHeaderHeight } from './functions/header-height';
 
-// Подключение плагина кастом-скролла
-// import 'simplebar';
-
 // Подключение плагина для позиционирования тултипов
 // import { createPopper, right} from '@popperjs/core';
 // createPopper(el, tooltip, {
 //   placement: 'right'
 // });
-
-// Подключение свайпера
-// import Swiper, { Navigation, Pagination } from 'swiper';
-// Swiper.use([Navigation, Pagination]);
-// const swiper = new Swiper(el, {
-//   slidesPerView: 'auto',
-// });
-
-// Подключение анимаций по скроллу
-// import AOS from 'aos';
-// AOS.init();
-
-// Подключение параллакса блоков при скролле
-// import Rellax from 'rellax';
-// const rellax = new Rellax('.rellax');
-
-// Подключение плавной прокрутки к якорям
-// import SmoothScroll from 'smooth-scroll';
-// const scroll = new SmoothScroll('a[href*="#"]');
-
-// Подключение событий свайпа на мобильных
-// import 'swiped-events';
-// document.addEventListener('swiped', function(e) {
-//   console.log(e.target);
-//   console.log(e.detail);
-//   console.log(e.detail.dir);
-// });
-
-// import { validateForms } from './functions/validate-forms';
-// const rules1 = [...];
-
-// const afterForm = () => {
-//   console.log('Произошла отправка, тут можно писать любые действия');
-// };
-
-// validateForms('.form-1', rules1, afterForm);
 
 /***/ }),
 
@@ -291,6 +254,162 @@ __webpack_require__.r(__webpack_exports__);
       });
     });
   }
+})();
+
+/***/ }),
+
+/***/ "./src/js/components/_filter.js":
+/*!**************************************!*\
+  !*** ./src/js/components/_filter.js ***!
+  \**************************************/
+/***/ (() => {
+
+(function () {
+  function filterPreInit(filterWrapper) {
+    const filterToggle = filterWrapper.querySelector('[data-filter-toggle]');
+    const filterMenu = filterWrapper.querySelector('[data-filter-menu]');
+    const filterItem = filterMenu.querySelectorAll('[data-filter-item]');
+    const filterClear = document.querySelector('[data-filter-clear]');
+    let optionsList = Array.from(filterMenu.children);
+    let optionsCount = optionsList.length;
+    let optionHoveredIndex = -1;
+    function openMenu() {
+      filterToggle.classList.toggle('active');
+      filterMenu.classList.toggle('active');
+      if (filterMenu.classList.contains('active')) {
+        filterToggle.setAttribute('aria-expanded', 'true');
+        if (filterToggle.getAttribute('data-filter-toggle') == 'type') {
+          filterToggle.setAttribute('aria-label', 'Закрыть фильтр «Тип»');
+        } else if (filterToggle.getAttribute('data-filter-toggle') == 'cooking-time') {
+          filterToggle.setAttribute('aria-label', 'Закрыть фильтр «Время приготовления»');
+        } else if (filterToggle.getAttribute('data-filter-toggle') == 'ingredients') {
+          filterToggle.setAttribute('aria-label', 'Закрыть фильтр «Количество ингредиентов»');
+        }
+        updateOption(optionHoveredIndex);
+
+        // добавляем прослушки действий
+        document.addEventListener('click', watchClickOutside);
+        document.addEventListener('keydown', supportKeyboardNavigation);
+      } else {
+        filterToggle.setAttribute('aria-expanded', 'false');
+        if (filterToggle.getAttribute('data-filter-toggle') == 'type') {
+          filterToggle.setAttribute('aria-label', 'Открыть фильтр «Тип»');
+        } else if (filterToggle.getAttribute('data-filter-toggle') == 'cooking-time') {
+          filterToggle.setAttribute('aria-label', 'Открыть фильтр «Время приготовления»');
+        } else if (filterToggle.getAttribute('data-filter-toggle') == 'ingredients') {
+          filterToggle.setAttribute('aria-label', 'Открыть фильтр «Количество ингредиентов»');
+        }
+        updateOption(-1);
+
+        // удаляем прослушки действий
+        document.removeEventListener('click', watchClickOutside);
+        document.removeEventListener('keydown', supportKeyboardNavigation);
+      }
+    }
+    function closeMenu() {
+      filterToggle.classList.remove('active');
+      filterMenu.classList.remove('active');
+      filterToggle.setAttribute('aria-expanded', 'false');
+      filterToggle.setAttribute('aria-label', 'Открыть меню действий');
+      updateOption(-1);
+
+      // удаляем прослушки действий
+      document.removeEventListener('click', watchClickOutside);
+      document.removeEventListener('keydown', supportKeyboardNavigation);
+    }
+    function updateOption(newIndex) {
+      optionsList = Array.from(filterMenu.children);
+      optionsCount = optionsList.length - 1;
+      const prevOption = filterMenu.children[optionHoveredIndex];
+      const option = filterMenu.children[newIndex];
+      if (prevOption) {
+        prevOption.classList.remove("focused");
+      }
+      if (option) {
+        option.classList.add("focused");
+      }
+      if (option != optionsList[optionsCount]) {
+        optionsList[optionsCount].classList.remove('focused');
+      }
+      optionHoveredIndex = newIndex;
+    }
+    function watchClickOutside(e) {
+      const target = e.target;
+      if (target !== filterToggle && target !== filterMenu) {
+        closeMenu();
+      }
+    }
+    function supportKeyboardNavigation(e) {
+      if (e.key === "ArrowDown" && filterMenu.classList.contains('active') && optionHoveredIndex < optionsCount) {
+        e.preventDefault();
+        updateOption(optionHoveredIndex + 1);
+      } else if (e.key === "ArrowDown" && filterMenu.classList.contains('active') && optionHoveredIndex >= optionsCount) {
+        e.preventDefault();
+        updateOption(0);
+      }
+      if (e.key === "ArrowUp" && filterMenu.classList.contains('active') && optionHoveredIndex > 0) {
+        e.preventDefault();
+        updateOption(optionHoveredIndex - 1);
+      } else if (e.key === "ArrowUp" && filterMenu.classList.contains('active') && optionHoveredIndex <= 0) {
+        e.preventDefault();
+        updateOption(optionsCount);
+      }
+      if (e.key === "Enter" && filterMenu.classList.contains('active')) {
+        e.preventDefault();
+
+        // https://codepen.io/sandrina-p/pen/yLprQgj?editors=1111
+
+        // const option = filterMenu.children[optionHoveredIndex];
+        // if (option) {
+        // 	const link = option.querySelector('button')
+        // 	const href = link.getAttribute('href')
+        // 	document.location = href
+        // }
+
+        closeMenu();
+      }
+      if (e.key === "Escape" || e.key === "Tab" && filterMenu.classList.contains('active')) {
+        closeMenu();
+      }
+    }
+    filterToggle.addEventListener('click', () => {
+      openMenu();
+    });
+    filterItem.forEach(item => {
+      const btn = item.querySelector('.filter-menu__btn');
+      const toggle = item.parentNode.parentNode.querySelector('[data-filter-toggle]');
+      item.addEventListener('click', e => {
+        e.preventDefault();
+        btn.classList.toggle('selected');
+        if (btn.classList.contains('selected')) {
+          toggle.classList.add('selected');
+          filterClear.removeAttribute('disabled');
+        } else {
+          toggle.classList.remove('selected');
+          filterClear.setAttribute('disabled', '');
+        }
+        closeMenu();
+      });
+    });
+    filterClear.addEventListener('click', () => {
+      const btns = filterMenu.querySelectorAll('.filter-menu__btn');
+      const toggle = document.querySelectorAll('.filter__toggle');
+      btns.forEach(el => {
+        el.classList.remove('selected');
+      });
+      toggle.forEach(el => {
+        el.classList.remove('selected');
+      });
+      filterClear.setAttribute('disabled', '');
+    });
+  }
+  function filterInit() {
+    const allFilterInPage = document.querySelectorAll('[data-filter]');
+    allFilterInPage.forEach(filterWrapper => {
+      filterPreInit(filterWrapper);
+    });
+  }
+  filterInit();
 })();
 
 /***/ }),
@@ -1561,7 +1680,7 @@ class GraphModal {
         }
       }.bind(this));
 
-      document.addEventListener('mousedown', function (e) {
+      document.addEventListener('click', function (e) {
         if (e.target.classList.contains('graph-modal') && e.target.classList.contains("is-open")) {
           this.close();
         }
@@ -1584,7 +1703,7 @@ class GraphModal {
     if (selector) {
       this.modalContainer = document.querySelector(`[data-graph-target="${selector}"]`);
     }
-
+    
     this.modalContainer.scrollTo(0, 0)
 
     this.modal.style.setProperty('--transition-time', `${this.speed / 1000}s`);
