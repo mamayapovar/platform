2b05a89d72d554bdd728bc28e60c15c21a83f725
Alexey Kalinin <72299738+sonniydsgn@users.noreply.github.com>
2023-05-28 23:49:45 +0500

    создание уведомлений

--- mamayapovar/static/recipes/js/main.js
+++ mamayapovar/static/recipes/js/main.js
@@ -36,13 +36,11 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _components_filter_js__WEBPACK_IMPORTED_MODULE_13___default = /*#__PURE__*/__webpack_require__.n(_components_filter_js__WEBPACK_IMPORTED_MODULE_13__);
 /* harmony import */ var _components_edit_js__WEBPACK_IMPORTED_MODULE_14__ = __webpack_require__(/*! ./components/_edit.js */ "./src/js/components/_edit.js");
 /* harmony import */ var _components_edit_js__WEBPACK_IMPORTED_MODULE_14___default = /*#__PURE__*/__webpack_require__.n(_components_edit_js__WEBPACK_IMPORTED_MODULE_14__);
-/* harmony import */ var _components_toast_js__WEBPACK_IMPORTED_MODULE_15__ = __webpack_require__(/*! ./components/_toast.js */ "./src/js/components/_toast.js");
-/* harmony import */ var _components_toast_js__WEBPACK_IMPORTED_MODULE_15___default = /*#__PURE__*/__webpack_require__.n(_components_toast_js__WEBPACK_IMPORTED_MODULE_15__);
 // import './components/_bookmark.js';
 // import './components/_like.js';
 // import './components/_subscribe.js';
 // import './components/_search-query.js';
-
+// import './components/_toast.js';
 
 
 
@@ -1320,70 +1318,6 @@ const scroll = new (smooth_scroll__WEBPACK_IMPORTED_MODULE_0___default())('a[hre
     });
   }
 })();
-
-/***/ }),
-
-/***/ "./src/js/components/_toast.js":
-/*!*************************************!*\
-  !*** ./src/js/components/_toast.js ***!
-  \*************************************/
-/***/ (() => {
-
-const notifications = document.querySelector('.notifications');
-let count = 0;
-const toastDetails = {
-  timer: 5000,
-  success: {
-    icon: "check"
-  },
-  info: {
-    icon: "info"
-  },
-  warning: {
-    icon: "warning"
-  },
-  error: {
-    icon: "warning"
-  }
-};
-function removeToast(toast) {
-  count--;
-  toast.classList.add('toast--hide');
-  setTimeout(() => toast.remove(), 200);
-}
-function createToast(id, text) {
-  const {
-    icon
-  } = toastDetails[id];
-  const toast = document.createElement('li');
-  toast.className = `toast  toast--${id}  toast--show`;
-  toast.innerHTML = `
-		<div class="toast__icon">
-			<svg class="icon  icon--16" aria-hidden="true" focusable="false">
-				<use href="img/sprite.svg#${icon}"/>
-			</svg>
-		</div>
-		<span class="toast__text">${text}</span>
-		<button class="toast__close  btn-reset" aria-label="Закрыть уведомление">
-			<svg class="icon  icon--16" aria-hidden="true" focusable="false">
-				<use href="img/sprite.svg#cross"/>
-			</svg>
-		</button>
-	`;
-  notifications.appendChild(toast);
-  count++;
-  if (count > 4) {
-    let topToast = document.querySelector('.toast');
-    removeToast(topToast);
-  }
-  setTimeout(() => removeToast(toast), toastDetails.timer);
-}
-document.addEventListener('click', e => {
-  const target = e.target;
-  if (target.classList.contains('toast__close')) {
-    removeToast(target.parentElement);
-  }
-});
 
 /***/ }),
 
