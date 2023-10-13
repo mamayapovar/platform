0b64e4d58b840cabed62001d047e50051db8db92
Крылов Николай <81432691+syab2@users.noreply.github.com>
2023-10-13 12:16:36 +0300

    Create package.py

---
+++ mamayapovar/package.py
@@ -0,0 +1,14 @@
+{
+  "name": "myap-platform",
+  "version": "0.6.0",
+  "description": "Мама, я повар! — это платформа, где люди могут делиться своими рецептами и обсуждать их с другими.",
+  "main": "index.js",
+	"repository": {
+    "type": "git",
+    "url": "https://github.com/mamayapovar/platform.git"
+  },
+  "scripts": {
+    "dev": "cd .utility && dev.bat"
+  },
+  "author": "Alexey Kalinin, Kolya Krylov"
+}
