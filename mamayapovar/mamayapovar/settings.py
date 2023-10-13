9176a734267e27d2699910075228860361b01331
Крылов Николай <81432691+syab2@users.noreply.github.com>
2023-10-13 12:10:57 +0300

    Update settings.py

--- mamayapovar/mamayapovar/settings.py
+++ mamayapovar/mamayapovar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(w*#q*w48sx!my!7c6+iz6^=5ff#)a=9(%s0#^r6kof&$)*nja
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
@@ -139,5 +139,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / "staticfiles" / "static"
 STATICFILES_DIRS = []
