995290c97e1892e9077373ad133cf77ddbc78908
Крылов Николай <81432691+syab2@users.noreply.github.com>
2023-10-13 12:20:00 +0300

    Update settings.py

--- mamayapovar/mamayapovar/settings.py
+++ mamayapovar/mamayapovar/settings.py
@@ -139,6 +139,5 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = BASE_DIR / "staticfiles" / "static"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = []
